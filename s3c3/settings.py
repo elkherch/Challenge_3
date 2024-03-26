fbfbf34cca2e83ff7ddec771133aeeaee9f4079b
elkherchi <22025@supnum.mr>
2024-03-26 07:24:13 +0000

    configuration backend

--- s3c3/settings.py
+++ s3c3/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-dn)p#!#j1qfey8ll&68zqj0s*yl9xme6-!8khag_$cs!#x&0n@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','supnumc3-78a493c7d339.herokuapp.com']
 
 
 # Application definition
